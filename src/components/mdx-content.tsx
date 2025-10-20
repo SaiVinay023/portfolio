090a34ecc4192d6c452974867330d0f21943cbfd
SaiVinay Manda <saivinay023@gmail.com>
2025-10-20 06:40:11 +0200

    fix: removed mdx components

--- src/components/mdx-content.tsx
+++ src/components/mdx-content.tsx
@@ -2,9 +2,9 @@
 'use client'
 
 import { useMDXComponent } from 'next-contentlayer/hooks'
-import type { MDXComponents } from 'mdx/types'
+//import type { MDXComponents } from 'mdx/types'
 
-type Props = { code: string; components?: MDXComponents }
+type Props = { code: string; components?: Record<string, React.ComponentType<any>> }
 
 export function MDXContent({ code, components = {} }: Props) {
   const Component = useMDXComponent(code)
